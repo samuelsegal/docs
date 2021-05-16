b4c5b61bb58352de710b1a2bbdc8bc1631aefeed
samuelsegal <samuelmosessegal@gmail.com>
2021-05-15 23:19:08 -0400

    fun learning rxjs

--- rxjs/rxjs-sandbox/src/domDraw.js
+++ rxjs/rxjs-sandbox/src/domDraw.js
@@ -1,4 +1,5 @@
 import { fromEvent } from 'rxjs';
+import { take, finalize, tap } from 'rxjs/operators';
 import _ from 'lodash';
 import './domDraw.css';
 
@@ -14,9 +15,6 @@ const draw = (x, y) => {
 	el.style.left = x + 'px';
 	document.body.appendChild(el);
 };
-const mousemoveObservable = fromEvent(document, 'mousemove');
-const mousedownObservable = fromEvent(document, 'mousedown');
-const mouseupObservable = fromEvent(document, 'mouseup');
 
 const mousedownObserver = {
 	next: (e) => {
@@ -27,23 +25,38 @@ const mousedownObserver = {
 	complete: () => console.log('complete'),
 };
 
+const mousemoveObservable = fromEvent(document, 'mousemove');
+const mouseupObservable = fromEvent(document, 'mouseup');
+const mousedownObservable = fromEvent(document, 'mousedown').pipe(
+	take(5),
+	tap(mousedownObserver),
+	finalize(() => {
+		console.log('5 attempts to draw have completed, unsubscribing mouseup and move upon downs take of 5');
+		mouseupSubscription.add(mousemoveSubscription);
+		mouseupSubscription.unsubscribe();
+		//mousemoveSubscription.unsubscribe();
+	})
+);
+
 const mousemoveObserver = {
 	next: (e) => {
+		console.log('dsfbsdfb');
 		if (drawNow) draw(e.clientX, e.clientY);
 	},
 	error: (e) => console.error(e),
-	complete: () => console.log('complete'),
+	complete: () =>
+		console.log('mouse move has completed, however unsubscribe does not trigger this? Still learning... fun stuff'),
 };
 
-const mousedownSubscription = mousedownObservable.subscribe(mousedownObserver);
+const mousedownSubscription = mousedownObservable.subscribe();
 const mouseupSubscription = mouseupObservable.subscribe((e) => (drawNow = false));
 const mousemoveSubscription = mousemoveObservable.subscribe(mousemoveObserver);
-
+//mousedownSubscription.add(mouseupSubscription);
+//mousemoveSubscription.add(mousedownSubscription);
 setTimeout((e) => {
-	mousedownSubscription.add(mouseupSubscription);
-	mousemoveSubscription.add(mousedownSubscription);
+	/* 	
 	mousedownObserver.complete();
 	mousemoveObserver.complete();
 	mousemoveObserver.error('asdfvaevdf');
-	mousemoveSubscription.unsubscribe();
+	mousemoveSubscription.unsubscribe(); */
 }, 5000);
